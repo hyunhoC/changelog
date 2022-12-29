5b8f5c10dc27e51cb2e889207edb3aa607338475
hyunhoC <whgusgh1357@gmail.com>
2022-12-29 21:33:56 +0900

    add auth endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import router from './router';
 import morgan from 'morgan';
+import { protect } from './modules/auth';
+import { createNewUser, signin } from './handlers/user';
 
 const app = express();
 
 app.use(morgan('dev')); // console logs time and size for the api response
 app.use(express.json()); // allows client to send json
-app.use(express.urlendcoded({extended: true})); // put query into object for you
+app.use(express.urlencoded({extended: true})); // put query into object for you
 
 app.get('/', (req, res) => {
   console.log('hello from express');
@@ -14,6 +16,9 @@ app.get('/', (req, res) => {
   res.json({message: 'hello'});
 });
 
-app.use('/api', router);
+app.use('/api', protect, router);
+
+app.post('/user', createNewUser);
+app.post('/signin', signin)
 
 export default app;
